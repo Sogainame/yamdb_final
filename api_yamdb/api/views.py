f8c2afb2dd7a6e6826f86fcffa17bc01e217ad5a
Fedor Shmurygin <fedorshmurygin@MacBook-Air-Fedor.local>
2021-12-24 09:53:03 +0700

    test1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -171,4 +171,4 @@ class UserViewSet(viewsets.ModelViewSet):
                 serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors,
-            status=status.HTTP_400_BAD_REQUEST)
+                        status=status.HTTP_400_BAD_REQUEST)
